e9712902e44f6172def3540b93770b729c0e1052
useriswave <121168867+useriswave@users.noreply.github.com>
2026-04-12 17:35:38 +0300

    matched the structure of TopLayout with the rest of the layout classes.

--- src/main/java/com/wave/waveutils/apputils/fileorganizer/layouts/TopLayout.java
+++ src/main/java/com/wave/waveutils/apputils/fileorganizer/layouts/TopLayout.java
@@ -13,90 +13,100 @@ import javafx.scene.layout.VBox;
 import java.awt.*;
 import java.net.URI;
 
-/*
- this class is re-used from my Wordle clone project, will continue to use this until further notice.
- https://github.com/useriswave/JavaFX-Wordle-Clone/tree/main
- */
-
 public class TopLayout extends StackPane {
 
+    private VBox layout;
+    private BorderPane bar;
+    private Label name;
+    private Label title;
+    private Button discord;
+    private Button github;
+    private Separator sep;
+    private HBox rightButtons;
+
     public TopLayout() {
+        initUI();
+        initStyles();
+        buttonEvents();
+        setIds();
+        registerElements();
+    }
 
-        VBox layout = new VBox();
+    private void initUI() {
+        name = new Label("@useriswave");
+        sep = new Separator();
+        github = new Button("GitHub");
+        discord = new Button("Discord");
+        bar = new BorderPane();
+        layout = new VBox();
+        title = new Label("File Organizer");
+        rightButtons = new HBox(10, discord, github);
+    }
 
-        BorderPane bar = new BorderPane();
-        bar.setPadding(new Insets(10, 30, 10, 30));
-
-        Label name = new Label("@useriswave");
-        Label title = new Label("File Organizer");
+    public void initStyles() {
         title.setStyle("-fx-font-weight: bold;\n" +
                 "-fx-font-size: 30;");
         title.setAlignment(Pos.CENTER);
         name.setStyle("-fx-font-weight: bold;\n" +
                 "-fx-font-size: 15;");
 
-        Button discord = new Button("Discord");
         discord.setStyle("-fx-background-color: #5865f2;" + "-fx-text-fill: white;" +
                 "-fx-font-weight: bold;" + "-fx-cursor: hand;" + "-fx-background-radius: 15px;");
         discord.setPadding(new Insets(10, 20, 10, 20));
 
-        Button github = new Button("GitHub");
         github.setStyle("-fx-background-color: black;" + "-fx-text-fill: white;" +
                 "-fx-font-weight: bold;" + "-fx-cursor: hand;" + "-fx-background-radius: 15px;");
         github.setPadding(new Insets(10, 20, 10, 20));
         discord.setFocusTraversable(false);
         github.setFocusTraversable(false);
 
-        hover(discord);
-        hover(github);
-        onDiscordButtonClick(discord);
-        onGithubButtonClick(github);
-
-        HBox rightButtons = new HBox(10, discord, github);
         rightButtons.setAlignment(Pos.CENTER_RIGHT);
-
-        Separator sep = new Separator();
-
+        bar.setPadding(new Insets(10, 30, 10, 30));
         bar.setLeft(name);
         bar.setRight(rightButtons);
+    }
 
-        StackPane.setAlignment(title, Pos.CENTER);
+    private void buttonEvents() {
+        openUrlOnButtonClick(github, "https://github.com/useriswave");
+        openUrlOnButtonClick(discord, "https://discord.com/users/971493987158482944");
+        applyHover(discord);
+        applyHover(github);
+    }
+
+    private void openUrlOnButtonClick(Button btn, String url) {
+        btn.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void applyHover(Button btn) {
+        btn.setOnMouseEntered( event-> {
+            btn.setOpacity(0.5);
+        });
+        btn.setOnMouseExited(e -> {
+            btn.setOpacity(1);
+        });
+    }
+
+    private void setIds() {
+        title.setId("top-title");
+        name.setId("top-name");
+        discord.setId("top-discord");
+        github.setId("top-discord");
+    }
+
+    public void registerElements() {
 
         layout.getChildren().addAll(bar, sep);
 
-        getChildren().addAll(layout, title);
-    }
-
-    private void hover(Button btn1) {
-        btn1.setOnMouseEntered( event-> {
-            btn1.setOpacity(0.5);
-        });
-        btn1.setOnMouseExited(e -> {
-            btn1.setOpacity(1);
-        });
-    }
-
-    private void onDiscordButtonClick(Button btn) {
-        btn.setOnAction(event -> {
-            try {
-                Desktop.getDesktop().browse(new URI("https://discord.com/users/971493987158482944"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
-
-    private void onGithubButtonClick(Button btn) {
-        btn.setOnAction(event -> {
-            try {
-                Desktop.getDesktop().browse(new URI("https://github.com/useriswave"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        this.getChildren().addAll(layout, title);
     }
 }
+
 
 
 
